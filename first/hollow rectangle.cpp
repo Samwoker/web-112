c43c05a664a4d0a62939e0c65c3f87894c373da5
Samwoker <149298507+Samwoker@users.noreply.github.com>
2023-12-15 22:46:47 -0900

    Update hollow rectangle.cpp

--- first/hollow rectangle.cpp
+++ first/hollow rectangle.cpp
@@ -1,32 +1,17 @@
 #include <iostream>
-
+using namespace std;
 int main() {
-    int rows, columns;
-
-    // Get the number of rows and columns from the user
-    std::cout << "Enter the number of rows: ";
-    std::cin >> rows;
-
-    std::cout << "Enter the number of columns: ";
-    std::cin >> columns;
-
-    // Outer loop for the number of rows
+    int rows=4, columns=6;
     for (int i = 1; i <= rows; ++i) {
-        // Inner loop for the number of columns
         for (int j = 1; j <= columns; ++j) {
-            // Check if it's the first or last row, or the first or last column
             if (i == 1 || i == rows || j == 1 || j == columns) {
-                // Print asterisk for the first and last rows, and first and last columns
-                std::cout << "* ";
+                cout << "* ";
             } else {
-                // Print space for the inner area
-                std::cout << "  ";
+                cout << "  ";
             }
         }
-        // Move to the next line after printing each row
-        std::cout << std::endl;
+        cout <<endl;
     }
-
     return 0;
 }
 
