a0a49d6a394482a8031f408c8d1b11ef5c7cbe15
Samwoker <149298507+Samwoker@users.noreply.github.com>
2023-12-15 22:48:02 -0900

    Update full pyramid.cpp

--- first/full pyramid.cpp
+++ first/full pyramid.cpp
@@ -1,26 +1,15 @@
 #include <iostream>
-
+using namespace std;
 int main() {
-    int rows;
-
-    // Get the number of rows from the user
-    std::cout << "Enter the number of rows for the full pyramid: ";
-    std::cin >> rows;
-
-    // Outer loop for the number of rows
+    int rows=6;
     for (int i = 1; i <= rows; ++i) {
-        // Inner loop for printing leading spaces
         for (int space = 1; space <= rows - i; ++space) {
-            std::cout << "  ";
+            cout << "  ";
         }
-
-        // Inner loop for printing asterisks
         for (int j = 1; j <= 2 * i - 1; ++j) {
-            std::cout << "* ";
+            cout << "* ";
         }
-
-        // Move to the next line after printing each row
-        std::cout << std::endl;
+        cout << endl;
     }
 
     return 0;
