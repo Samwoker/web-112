48de71730852aeef290d7e26f9766e945fe611df
Samwoker <149298507+Samwoker@users.noreply.github.com>
2023-12-15 22:43:13 -0900

    Update inverted hollow full pyramid.cpp

--- first/inverted hollow full pyramid.cpp
+++ first/inverted hollow full pyramid.cpp
@@ -1,32 +1,19 @@
 #include <iostream>
-
+using namespace
 int main() {
-    int rows;
-
-    // Get the number of rows from the user
-    std::cout << "Enter the number of rows for the inverted hollow full pyramid: ";
-    std::cin >> rows;
-
-    // Outer loop for the number of rows
+    int rows=6;
     for (int i = rows; i >= 1; --i) {
-        // Inner loop for printing leading spaces
         for (int space = 1; space <= rows - i; ++space) {
             std::cout << "  ";
         }
-
-        // Inner loop for printing asterisks or spaces
         for (int j = 1; j <= 2 * i - 1; ++j) {
             if (j == 1 || j == 2 * i - 1 || i == rows) {
-                // Print asterisk for the first and last columns or the last row
-                std::cout << "* ";
+                cout << "* ";
             } else {
-                // Print space for the inner area
-                std::cout << "  ";
+                cout << "  ";
             }
         }
-
-        // Move to the next line after printing each row
-        std::cout << std::endl;
+        cout <<endl;
     }
 
     return 0;
