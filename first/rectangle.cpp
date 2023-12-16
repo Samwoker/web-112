b8e826d1b08bb7f0c8fb1b707b327be034fb5a5c
Samwoker <149298507+Samwoker@users.noreply.github.com>
2023-12-15 22:44:53 -0900

    Update rectangle.cpp

--- first/rectangle.cpp
+++ first/rectangle.cpp
@@ -1,24 +1,12 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int rows, columns;
-
-    // Get the number of rows and columns from the user
-    std::cout << "Enter the number of rows: ";
-    std::cin >> rows;
-
-    std::cout << "Enter the number of columns: ";
-    std::cin >> columns;
-
-    // Outer loop for the number of rows
+    int rows=4, columns=6;
     for (int i = 1; i <= rows; ++i) {
-        // Inner loop for the number of columns
         for (int j = 1; j <= columns; ++j) {
-            // Print an asterisk for each column
-            std::cout << "* ";
+            cout << "* ";
         }
-        // Move to the next line after printing each row
-        std::cout << std::endl;
+    cout << endl;
     }
 
     return 0;
